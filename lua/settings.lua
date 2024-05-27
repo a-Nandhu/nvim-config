d6c5b0364cbfeb379727eec52048102a58dcce89
Nandhu <nxndhu@gmail.com>
2024-05-27 17:32:13 +0530

    Initial Commit

---
+++ lua/settings.lua
@@ -0,0 +1,12 @@
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.scrolloff = 8
+
+vim.g.mapleader = " "
+
+vim.cmd("set number")
+
+vim.keymap.set("n","<leader>pv", ":Ex<CR>")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv")
+vim.api.nvim_set_keymap('n', '<C-/>', ':ToggleTerm<CR>', { noremap = true, silent = true })
