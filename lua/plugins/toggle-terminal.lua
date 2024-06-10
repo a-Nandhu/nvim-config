5e827f5536c46ca15fc085d08786b8c845649847
a-Nandhu <nxndhu@gmail.com>
2024-06-10 10:48:12 +0530

    updated settings and toggle-term

--- lua/plugins/toggle-terminal.lua
+++ lua/plugins/toggle-terminal.lua
@@ -1,8 +1,19 @@
-return{
-	"akinsho/toggleterm.nvim",
-    config = function()
-      	require("toggleterm").setup({})
-    end,
-
-	vim.keymap.set("n","<C-t>",":ToggleTerm direction=float<CR>")
+return {
+	{
+		"akinsho/toggleterm.nvim",
+		config = true,
+		cmd = "ToggleTerm",
+		keys = {
+			{ "<C-t>", "<cmd>ToggleTerm<cr>", desc = "Toggle floating terminal" }
+		},
+		opts = {
+			open_mapping = [[<C-t>]],
+			direction = "float",
+			shade_filetypes = {},
+			hide_numbers = true,
+			insert_mappings = true,
+			terminal_mappings = true,
+			start_in_insert = true,
+			close_on_exit = true, },
+	},
 }
--- lua/settings.lua
+++ lua/settings.lua
@@ -1,12 +1,16 @@
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.scrolloff = 8
+vim.opt.mouse = ""
 
 vim.g.mapleader = " "
-
+vim.g.netrw_banner = 0 
 vim.cmd("set number relativenumber")
 
 vim.keymap.set("n","<leader>pv", ":Ex<CR>")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv")
-vim.api.nvim_set_keymap('n', '<C-/>', ':ToggleTerm<CR>', { noremap = true, silent = true })
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
